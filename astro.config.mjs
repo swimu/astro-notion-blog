4b6ba1aafe8913ad3bc3015e8f30db1ca2f6312f
swimu <swimu1003@gmail.com>
2024-06-29 01:09:19 +0900

    astro.config.mjsの差分を取り込む

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,21 @@ import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
-import vercel from '@astrojs/vercel/serverless';
+
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
   }
+
   if (process.env.VERCEL && process.env.VERCEL_URL) {
     return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
   }
+
   if (process.env.CF_PAGES) {
     if (process.env.CF_PAGES_BRANCH !== 'main') {
       return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
     }
+
     return new URL(
       BASE_PATH,
       `https://${new URL(process.env.CF_PAGES_URL).host
@@ -29,8 +32,6 @@ const getSite = function () {
 };
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   site: getSite(),
   base: BASE_PATH,
@@ -40,6 +41,4 @@ export default defineConfig({
     FeaturedImageDownloader(),
     PublicNotionCopier(),
   ],
-  output: 'server',
-  adapter: vercel(),
 });
